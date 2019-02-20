9ef5ade1a39aa99eb983a306ebd41eb8bc749a18
Manuel <manuel.diaz@svcfin.com>
2019-02-20 10:48:00 -0500

    knockout debugging tips

---
+++ Knockout_Ex/Knockout_Ex/Scripts/app/dump.js
@@ -0,0 +1,21 @@
+﻿
+ko.bindingHandlers.dump = {
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var context = valueAccessor();
+        var allBindings = allBindings();
+        var pre = document.createElement('pre');
+
+        element.appendChild(pre);
+
+        var dumpJSON = ko.computed({
+            read: function () {
+                return ko.toJSON(context, null, 2);
+            },
+            disposeWhenNodeIsRemoved: element
+        });
+
+        ko.applyBindingsToNode(pre,
+            { text: dumpJSON }
+        );
+    }
+}
